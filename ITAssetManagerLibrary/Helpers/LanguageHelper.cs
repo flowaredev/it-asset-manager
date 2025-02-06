0f86b7cf85b57245b7f09a1d22eaf4ace9a6291f
이호철 <charles@floware.xyz>
2025-02-06 21:35:25 +0900

    Centralize strings with LanguageHelper, update .razor files
    Replaced hardcoded strings with LanguageHelper references for titles and labels across various .razor files to facilitate localization. Removed @attribute [StreamRendering] directive from multiple files. Updated HeaderTitle component to use LanguageHelper properties. Removed Title parameter from CommonAssetDataGrid component. Added using directive for ITAssetManagerLibrary.Helpers in _Imports.razor. Updated NavMenu.razor to use LanguageHelper for menu labels. Changed BaseDataGrid.razor to use hardcoded "장애" instead of LanguageHelper.FaultTitle. Expanded LanguageHelper with new string properties. Removed HeaderTitle component from ServiceLevelDataGrid.razor.

--- ITAssetManagerLibrary/Helpers/LanguageHelper.cs
+++ ITAssetManagerLibrary/Helpers/LanguageHelper.cs
@@ -9,5 +9,16 @@ namespace ITAssetManagerLibrary.Helpers
     public class LanguageHelper
     {
         public const string FaultTitle = "장애조치";
+        public const string VulnerabilityTitle = "취약점조치";
+        public const string ScheduledMaintenanceTitle = "정기점검";
+        public const string MaintenanceTitle = "유지보수";
+        public const string ServerTitle = "서버";
+        public const string StorageTitle = "저장장치";
+        public const string BackupEquipmentTitle = "백업장비";
+        public const string NetworkEquipmentTitle = "네트워크";
+        public const string SecurityEquipmentTitle = "보안장비";
+        public const string SoftwareTitle = "소프트웨어";
+        public const string SupportEquipmentTitle = "부대장비";
+        public const string EtcTitle = "기타";
     }
 }
