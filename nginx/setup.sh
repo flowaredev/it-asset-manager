2947831498c7f5e2182676905a5202c9d869992c
이호철 <charles@floware.xyz>
2025-08-07 17:26:17 +0900

    Update kestrel-itsm service and setup script
    Modified the `kestrel-itsm.service` file to change the `ExecStart` command for the dotnet executable.
    
    Enhanced `setup.sh` by adding commands to set SELinux to permissive mode, copy the service file, enable and start the `kestrel-itsm` and `nginx` services, check their statuses, and configure firewall rules for port 5050. Included steps to create and load a custom SELinux policy and restart both services to apply changes.

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-cp ./kestrel-itsm.service /etc/systemd/system/kestrel-helloapp.service
+setenforce 0
+cp ./kestrel-itsm.service /etc/systemd/system/kestrel-itsm.service
 systemctl enable kestrel-itsm.service
 systemctl start kestrel-itsm.service
 systemctl status kestrel-itsm.service
 systemctl enable nginx
 systemctl start nginx
 systemctl status nginx
+
+sudo ausearch -m avc -ts recent | sudo audit2allow -M itsm-dotnet-policy
+sudo semodule -i itsm-dotnet-policy.pp
+
+sudo firewall-cmd --permanent --zone=public --add-port=5050/tcp
+sudo firewall-cmd --reload
+sudo firewall-cmd --list-ports
+
+systemctl restart kestrel-itsm.service
+systemctl restart nginx
